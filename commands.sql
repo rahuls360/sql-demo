062ddd7a40b548f9f406106491924147b92f334c
Rahul Makhija <rahuls360@gmail.com>
2024-04-20 11:32:28 +0530

    till lesson 4 complete

--- commands.sql
+++ commands.sql
@@ -20,5 +20,26 @@ SELECT DISTINCT city from users;
 SELECT * FROM users ORDER BY username DESC;
 -- WHERE with multiple conditions (AND)
 SELECT * FROM users WHERE id > 0 AND city="Bangalore";
--- filter by string (LIKE. % is for till the end. OR operator)
+-- filter by string (LIKE = Case insensitive exact string comparison. % is used in a string, to match 0 or more charectors. OR operator)
 SELECT * FROM users WHERE city="Bangalore" OR username LIKE 'r%';
+
+-- movies released b/w 2010 and 2010 (BETWEEN -> WHERE column_name BETWEEN … AND …)
+SELECT * FROM movies WHERE year BETWEEN 2000 AND 2010;
+SELECT * FROM movies WHERE year >= 2000 AND year <= 2010;
+
+-- movies not released b/w 2010 and 2010 (BETWEEN -> WHERE column_name NOT BETWEEN … AND …)
+SELECT * FROM movies WHERE year NOT BETWEEN 2000 AND 2010;
+SELECT * FROM movies WHERE year <= 2000 OR year >= 2010;
+
+-- string contains text at start -> WHERE column_name LIKE "stringToMatch"
+SELECT * FROM movies WHERE title LIKE "Toy story%";
+-- not equals
+SELECT * FROM movies WHERE director !="John Lasseter";
+-- get distinct directors and order in ascending order (alpha numeric sort)
+SELECT DISTINCT director FROM movies ORDER BY director;
+-- order movies in descending order of year. return first 4 results
+SELECT * FROM movies ORDER BY year DESC LIMIT 4;
+-- order by multiple columns (ORDER BY column_name ASC/DESC, column_name2 ASC/DEC )
+SELECT * FROM movies ORDER BY title ASC, year ASC LIMIT 5;
+-- show paginated response (OFFSET)
+SELECT * FROM movies ORDER BY title ASC, year ASC LIMIT 5 OFFSET 5;
